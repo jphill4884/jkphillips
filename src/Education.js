b0700c9668541dba33d22e52f1b38992126d10da
justin.phillips <justin.phillips@HCP-J4YYPXM0PF.local>
2023-04-22 13:32:04 -0700

    add tablet styling

--- src/Education.js
+++ src/Education.js
@@ -1,14 +1,22 @@
 import "./Main.css";
+import { format, parseISO } from "date-fns";
 
 function Education(props) {
-
-  
-return (
-    <div className="job">
-      <div className="school">{props.props.fields.educationName}</div>
+  return (
+    <div className="education">
+      <div className="company">{props.props.fields.educationName}</div>
       <div className="title">{props.props.fields.courseType}</div>
-      <div className="location">{props.props.fields.startDate} - {props.props.fields.endDate}</div>
-      <div className="responsibilities"></div>
+      <div className="date-row">
+        <div className="date">
+          {format(parseISO(props.props.fields.startDate), "MMMM yyyy")}
+        </div>
+        <div className="date">through</div>
+        <div className="date">
+          {!props.props.fields.endDate
+            ? "present"
+            : format(parseISO(props.props.fields.endDate), "MMMM yyyy")}
+        </div>
+      </div>
     </div>
   );
 }
